72914128f631e962f29d8d51423f57cf85383ce9
Jose Cruz-Toledo <josemiguelcruztoledo@gmail.com>
2014-10-27 20:09:52 -0700

    added cli parser and examples

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ RDFtoCXLConverter
 =================
 
 A simple script to convert an RDF document to a CXL file to view with CmapTools http://cmap.ihmc.us/
+
+#Usage
+`python cxl_converter.py -i input.rdf -o /tmp/out.cxl`
--- cxl_converter.py
+++ cxl_converter.py
@@ -3,6 +3,7 @@
 import rdflib
 import hashlib
 import sys
+import argparse
 from lxml import etree
 from lxml import objectify
 from random import randrange
@@ -12,32 +13,40 @@ from random import randrange
 Create CXL files from RDF files - 
 """
 
-MY_NAMESPACES={
+parser = argparse.ArgumentParser(
+	description = __doc__,
+	formatter_class = argparse.RawDescriptionHelpFormatter
+)
+#add arguments
+parser.add_argument('-i','--input_file',help='path to input RDF file', required=True)
+parser.add_argument('-o', '--output_file', help='path to output CXL file', required=True)
+
+NS_MAP={
 	'dc': 'http://purl.org/dc/elements/1.1/', 
 	 None: 'http://cmap.ihmc.us/xml/cmap/'}
 
 
 def make_lxml_string(query_results):
 
-	r=etree.Element('cmap', nsmap=MY_NAMESPACES)
-	res_meta=etree.Element('{%s}res-meta' % MY_NAMESPACES[None])
-	title=etree.Element('{%s}title' % MY_NAMESPACES['dc'])
+	r=etree.Element('cmap', nsmap=NS_MAP)
+	res_meta=etree.Element('{%s}res-meta' % NS_MAP[None])
+	title=etree.Element('{%s}title' % NS_MAP['dc'])
 	title.text = "SIEVE II data model"
-	description = etree.Element('{%s}description' %MY_NAMESPACES['dc'])
+	description = etree.Element('{%s}description' %NS_MAP['dc'])
 	description.text = "CMAP version of data model"
 	res_meta.append(title)
 	res_meta.append(description)
 	r.append(res_meta)
 	#add map node
-	map_node = etree.Element('{%s}map' %MY_NAMESPACES[None])
+	map_node = etree.Element('{%s}map' %NS_MAP[None])
 	#create concept list
-	concept_list = etree.Element('{%s}concept-list' %MY_NAMESPACES[None])
+	concept_list = etree.Element('{%s}concept-list' %NS_MAP[None])
 	cl = []
 	#creaate linking-phrase-list
-	linking_phrase_list = etree.Element('{%s}linking-phrase-list' %MY_NAMESPACES[None])
+	linking_phrase_list = etree.Element('{%s}linking-phrase-list' %NS_MAP[None])
 	pl = []
 	#create connection list
-	connection_list = etree.Element('{%s}connection-list' %MY_NAMESPACES[None])
+	connection_list = etree.Element('{%s}connection-list' %NS_MAP[None])
 	connl = []
 
 	for s, p, o in query_results:
@@ -52,22 +61,22 @@ def make_lxml_string(query_results):
 		cl.append((olbl,obj_md5))
 		pl.append((plbl,pred_md5))
 
-		conn = etree.Element('{%s}connection' %MY_NAMESPACES[None])
+		conn = etree.Element('{%s}connection' %NS_MAP[None])
 		conn.set('id',str(randrange(100000)))
 		conn.set('from-id', sub_md5)
 		conn.set('to-id', pred_md5)
-		conn2 = etree.Element('{%s}connection' %MY_NAMESPACES[None])
+		conn2 = etree.Element('{%s}connection' %NS_MAP[None])
 		conn2.set('from-id', pred_md5)
 		conn2.set('to-id', obj_md5)
 		connection_list.append(conn)
 		connection_list.append(conn2)
 
 	for l in cl:
-		concept = etree.Element('{%s}concept' %MY_NAMESPACES[None], id=str(l[1]), label=str(l[0]))
+		concept = etree.Element('{%s}concept' %NS_MAP[None], id=str(l[1]), label=str(l[0]))
 		concept_list.append(concept)
 
 	for p in pl:
-		lp = etree.Element('{%s}linking-phrase' %MY_NAMESPACES[None], id=str(p[1]), label=str(p[0]))
+		lp = etree.Element('{%s}linking-phrase' %NS_MAP[None], id=str(p[1]), label=str(p[0]))
 		linking_phrase_list.append(lp)
 
 	map_node.append(concept_list)
@@ -78,11 +87,12 @@ def make_lxml_string(query_results):
 	return s
 
 
-def main():
+def main(argv):
+	ar = parser.parse_args(argv[1:])
 	#path to local rdf file that you wish to convert to cxl
-	input_rdf_file = "input.nt"
+	input_rdf_file = ar.input_file
 	#path to output cxl file to be opened with cxl
-	output_cxl_file = "output.cxl"
+	output_cxl_file = ar.output_file
 
 
 	g = rdflib.Graph()
@@ -103,4 +113,4 @@ def main():
 	text_file.close()
 
 if __name__ == "__main__":
-	main()
+	main(sys.argv)
